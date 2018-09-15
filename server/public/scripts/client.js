8e6655f670496a3f38a19b690a6892a110a172aa
Daniel Ridley <ridleydan31@gmail.com>
2018-09-15 15:44:25 -0500

    append tasks to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,34 +1,37 @@
 const taskApp = angular.module('TaskApp', []);
-self.objectToSend = {};
-self.tasks = [];
-taskApp.controller('TaskController', ['$http', function ($http) {
-    self = this;
-    self.message = ('Well, hello there.');
 
-    self.addTask = function () {
-        console.log(self.objectToSend);
-        $http({
-            method: 'POST',
-            url: '/tasks',
-            data: self.objectToSend
-        }).then(function (response) {
-            console.log('Back from Server with POST', response);
-            self.objectToSend = {};
-        }).catch(function (error) {
-            alert('Unable to add Task', error);
-            console.log('Error', error);
-        });
-    }
+taskApp.controller('TaskController', ['$http', function ($http) {
+    const self = this;
+    self.message = ('Well, hello there.');
+    self.objectToSend = {};
     self.getTask = function () {
         $http({
             method: 'GET',
             url: '/tasks'
         }).then(function (response) {
             console.log('Back from Server with GET', response);
-            self.tasks = response.data;
-        }).catch(function(error) {
+            self.tasksArray = response.data;
+        }).catch(function (error) {
             alert('Unable to GET from Server', error);
             console.log('Error', error);
         })
+    };
+    self.addTask = function (newTask) {
+        console.log(self.objectToSend);
+        $http({
+            method: 'POST',
+            url: '/tasks',
+            data: newTask
+        }).then(function (response) {
+            console.log('Back from Server with POST', response);
+            self.objectToSend = {}
+            self.getTask();
+        }).catch(function (error) {
+            alert('Unable to add Task', error);
+            console.log('Error', error);
+        });
     }
+
+    self.getTask();
+
 }]);
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const PORT = 5000;
 //uses
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 //spins
 app.listen(PORT, () => {
     console.log('SERVER UP AND RUNNING ON PORT: ', PORT);
@@ -16,7 +17,7 @@ app.listen(PORT, () => {
 
 //connect to Mongo via mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/todos', ({useNewUrlParser: true}));
+mongoose.connect('mongodb://localhost:27017/todos', ({ useNewUrlParser: true }));
 
 mongoose.connection.on('connected', () => {
     console.log('Connected to Mongo');
@@ -27,17 +28,15 @@ mongoose.connection.on('error', (error) => {
 })
 
 const Schema = mongoose.Schema;
-
-
-const TaskSchema = new Schema({
-    description: { type: String },
+const taskSchema = new Schema({
+    task: { type: String },
     category: { type: String },
-    completed: { type: Boolean }
+    completed: { type: String }
 });
-const Task = mongoose.model('Tasks', TaskSchema);
+const Task = mongoose.model('tasks', taskSchema);
+
 app.post('/tasks', (req, res) => {
     console.log('in /tasks POST');
-    console.log('HEEYYYYY LOOOK', req.body);
     Task.create(req.body).then(() => {
         console.log('Item added', req.body);
         res.sendStatus(201);
@@ -45,4 +44,14 @@ app.post('/tasks', (req, res) => {
         console.log(error);
         res.sendStatus(500);
     });
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then(function (response) {
+        console.log('Hey, LOOOK', response);
+        res.send(response);
+    }).catch((error) => {
+        console.log("Error in server .find", error);
+        res.sendStatus(500);
+    })
 })
